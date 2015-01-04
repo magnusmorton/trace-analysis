01de276a4189cc7fc69df58f83b58df55342c832
Magnus Morton <magnusmorton@gmail.com>
2015-01-04 23:20:06 +0000

    added simple benchmark

---
+++ sumupto.rkt
@@ -0,0 +1,14 @@
+#lang pycket #:stdlib
+
+
+
+(define enumFromTo
+  (lambda (m n)
+    (if (> m n) '() (cons m (enumFromTo (+ m 1) n)))))
+
+(define sum
+  (lambda (_list)
+    (foldl (lambda (s x) (+ s x)) 0 _list)))
+
+
+(time (printf "~a\n" (sum  (enumFromTo 1 10000000))))
