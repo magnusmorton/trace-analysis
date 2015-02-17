833cf3771db0e4eb23ce97846b21087d127d42c1
Magnus Morton <magnusmorton@gmail.com>
2015-02-17 16:46:25 +0000

    added basic matching

--- result_parser.py
+++ result_parser.py
@@ -1,4 +1,12 @@
+import re
 import sys
 
+loop_re = re.compile("LOOP - HASH: (.*) TT: (.*) COST: (.*)")
+bridge_re = re.compile("BRIDGE - HASH: (.*) GUARD: (.*) COST: (.*)")
 for arg in sys.argv:
     print arg
+    with open(arg, 'r') as f:
+        for line in f:
+            m = loop_re.match(line)
+            print m
+    
