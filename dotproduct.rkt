dfab00abe9c07cd6f864f697d9baf9d573398cc5
Magnus Morton <magnusmorton@gmail.com>
2015-02-26 00:12:14 +0000

    fixed dot product

--- dotproduct.rkt
+++ dotproduct.rkt
@@ -1,14 +1,26 @@
 #lang pycket #:stdlib
 
 (define SIZE (string->number (vector-ref (current-command-line-arguments) 0 )))
-(define vec1 (build-list SIZE values))
-(define vec2 (build-list SIZE values))
-(define (dot-product l r) 
-  (if (= 0 (length r)) 0 
+
+(define (mk-list size v)
+  (if (= 0 size)
+      null
+      (cons v (mk-list (- size 1) v))
+      )
+  )
+
+(define vec1 (mk-list SIZE 3))
+(define vec2 (mk-list SIZE 5))
+
+
+(define (dot-product l r)
+  (if (= 0 (length r)) 0
       ( + (* (car l) (car r)) (dot-product (cdr l) (cdr r)))
       )
 )
- 
- 
+
+
+
+
 ;; dot-product works on sequences such as vectors:
 (time (dot-product vec1 vec2))
