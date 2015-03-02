bfce3a5cce43bb72aa8654d46b5d070d23d38549
Magnus Morton <j.morton.2@research.gla.ac.uk>
2015-03-02 11:58:45 +0000

    new runners

--- runproper.sh
+++ runproper.sh
@@ -7,7 +7,7 @@ do
     echo $bench
     for i in `seq 10`
     do
-	(perf stat ./pycket-c trace-benches/sumupto_silly.rkt $bench)  &>> sumupto_silly${bench}
+	(perf stat ./pycket-c trace-benches/sumupto-proper.rkt $bench)  &>> sumupto-proper${bench}
     done
 done
 exit 0 
