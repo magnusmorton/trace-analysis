b80086f59379be5582120ee7d030cb09583dedbf
Magnus Morton <m@magnusmorton.com>
2015-04-23 16:23:26 +0100

    general structure of macro complete

--- arith.rkt
+++ arith.rkt
@@ -2,7 +2,7 @@
 
 
 (define SIZE (string->number (vector-ref (current-command-line-arguments) 0)))
-(define REPS (string->number (vector-ref (current-command-line-arguments) 1)))
+;(define REPS (string->number (vector-ref (current-command-line-arguments) 1)))
 
 (define out null)
 
@@ -13,14 +13,19 @@
 (require (for-syntax racket/syntax))
 (require (for-syntax racket/base))
 
+(define-for-syntax ops '(+ - * /))
+
 (define-syntax (test stx)
   (syntax-case stx ()
     [( _ type )
-     #'(for ([i  vec1])
-               (let* ([x i]
+     #`(for ([i  vec1])
+               (let* (
+                      #,@(for/list ([x (in-range 10)]))
+                      [x i]
                       [y (+ 3 x)]
                       [z (* y x)]
-                      [a (+ z y)])
+                      [a (+ z y)]
+                      )
                  (set! out a)))]))
 
 
