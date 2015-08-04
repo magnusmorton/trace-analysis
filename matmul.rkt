eab12507ddbdbfa1527765488af2cef52f52a914
Magnus Morton <magnusmorton@icloud.com>
2015-08-04 21:40:08 +0100

    sortof works

--- matmul.rkt
+++ matmul.rkt
@@ -10,15 +10,27 @@
   (for/list ([c (apply map list m2)])
     (apply + (map * r c))))
 
-(define (m-mult-chunked m1 m2 n)
-  (let* ([len (length m1)]
-         [x (m-mult (slice m1 0 (/ len n)))]
-         [y (m-mult (slice m1 2 (/ len n)))])
-    (append x y)))
-;;(m-mult '((1 2) (3 4)) '((5 6) (7 8)))
-
-(m-mult test1 test2)
-;; -> '((19 22) (43 50))
-
 (define (slice l offset n)
   (take (drop l offset) n))
+
+(define (m-mult-chunked m1 m2 n)
+  (let* ([len (length m1)]
+         [x (m-mult (slice m1 0 (/ len n)) m2)]
+         [y (m-mult (slice m1 2 (/ len n)) m2)])
+    (append x y)))
+(m-mult test1 test2)
+
+(m-mult-chunked test1 test2 2)
+;; -> '((19 22) (43 50))
+
+
+
+(define (make-mat rows cols)
+  (for/list ([i cols])
+    (random-list rows)))
+
+(define (random-list size)
+  (for/list ([i size])
+    (random 10)))
+
+(length (make-mat 10 10))
