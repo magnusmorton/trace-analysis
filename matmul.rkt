41d79df7287b510ba29d91c2763cae1fc8b25055
Magnus Morton <magnusmorton@icloud.com>
2015-08-04 20:45:54 +0100

    refactored

--- matmul.rkt
+++ matmul.rkt
@@ -1,8 +1,11 @@
 #lang racket
 (define (m-mult m1 m2)
   (for/list ([r m1])
-    (for/list ([c (apply map list m2)])
-      (apply + (map * r c)))))
+    (m-mult-inner r m2)))
+
+(define (m-mult-inner r m2)
+  (for/list ([c (apply map list m2)])
+    (apply + (map * r c))))
 
 (define (m-mult-chunked m1 m2 n) '())
 (m-mult '((1 2) (3 4)) '((5 6) (7 8)))
