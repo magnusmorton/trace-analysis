91cbf62dd56e813f66eab2004c2eadddbc30999d
Magnus Morton <magnusmorton@icloud.com>
2015-08-04 21:17:44 +0100

    chunked mat mul

--- matmul.rkt
+++ matmul.rkt
@@ -1,4 +1,7 @@
 #lang racket
+
+(define test1 '((1 2 3 4) (1 2 3 4) (1 2 3 4) (1 2 3 4)))
+(define test2 '((5 6 7 8) (5 6 7 8) (5 6 7 8) (5 6 7 8)))
 (define (m-mult m1 m2)
   (for/list ([r m1])
     (m-mult-inner r m2)))
@@ -7,6 +10,15 @@
   (for/list ([c (apply map list m2)])
     (apply + (map * r c))))
 
-(define (m-mult-chunked m1 m2 n) '())
-(m-mult '((1 2) (3 4)) '((5 6) (7 8)))
+(define (m-mult-chunked m1 m2 n)
+  (let* ([len (length m1)]
+         [x (m-mult (slice m1 0 (/ len n)))]
+         [y (m-mult (slice m1 2 (/ len n)))])
+    (append x y)))
+;;(m-mult '((1 2) (3 4)) '((5 6) (7 8)))
+
+(m-mult test1 test2)
 ;; -> '((19 22) (43 50))
+
+(define (slice l offset n)
+  (take (drop l offset) n))
