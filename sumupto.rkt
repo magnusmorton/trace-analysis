d4b7d9874f2df8b9cb3939c6f4de7aaa36c7f8de
Magnus Morton <m@magnusmorton.com>
2015-08-06 14:37:15 +0100

    removed sum function

--- sumupto.rkt
+++ sumupto.rkt
@@ -6,11 +6,5 @@
   (lambda (m n)
     (if (> m n) '() (cons m (enumFromTo (+ m 1) n)))))
 
-(define sum
-  (lambda (_list)
-    (foldl (lambda (s x) (+ s x)) 0 _list)))
-
-
-
 (time (enumFromTo 1 SIZE))
 
