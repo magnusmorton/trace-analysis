a1d0d3baaa3ea59eea5a5d87188feba14acd1bdc
Magnus Morton <magnusmorton@icloud.com>
2015-08-17 14:17:46 +0100

    pulled transpose out of the loop

--- matmul_vec.rkt
+++ matmul_vec.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-(define WORKERS (string->number (vector-ref (current-command-line-arguments) 0 )))
+;;(define WORKERS (string->number (vector-ref (current-command-line-arguments) 0 )))
 (define (random-vector size)
   (for/vector ([i size])
     (random 10)))
@@ -10,11 +10,12 @@
     (random-vector rows)))
 
 (define (matmul m1 m2)
-  (for/vector ([r m1])
-    (for/vector ([c (apply vector-map vector (vector->list m2))])
-      (for/fold ([sum 0])
-                ([i (vector-map * r c)])
-        (+ sum i)))))
+  (let ([transpose (apply vector-map vector (vector->list m2))]) 
+    (for/vector ([r m1])
+      (for/vector ([c transpose])
+        (for/fold ([sum 0])
+                  ([i (vector-map * r c)])
+          (+ sum i))))))
 
 (define (matmul-chunked m1 m2 n)
   (let ([chunk (/ (vector-length m1) n)])
@@ -29,11 +30,11 @@
 ;;(time (matmul-chunked (make-mat 1000 100) (make-mat 100 1000) WORKERS))
 
 ;; warmup
-(matmul mat1 mat2)
+;;(matmul mat1 mat2)
 
 
 ;;actual
-(matmul mat1 mat2)
+(time (matmul mat1 mat2))
 
 ;; actual chunked
 ;;(matmul-chunked mat1 mat2 10)
