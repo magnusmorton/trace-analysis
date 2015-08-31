a0dd7a4496b0457ed44c15e3ed7002bdcb973087
Magnus Morton <magnusmorton@icloud.com>
2015-09-01 00:14:40 +0100

    cmd line internface

--- sumeuler.rkt
+++ sumeuler.rkt
@@ -1,5 +1,25 @@
 #lang racket/base
 
+
+(require racket/cmdline)
+
+(define chunk-size (make-parameter 0))
+(define task? (make-parameter #f))
+(define stride? (make-parameter #f))
+(define len (make-parameter 4000))
+
+(command-line
+ #:program "sumeuler"
+ #:once-each
+ [("-t" "--task") "task?"
+  (task? #t)]
+ [("-c" "--chunk-size") cs "chunk size"
+  (chunk-size (string->number cs))]
+ [("-s" "--stride") "stride?"
+  (stride #t)]
+ [("-l" "--length") len "length"
+  (len (string->number len))])
+ 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Sum of Euler's totient function
 
@@ -8,6 +28,7 @@
 
 ;; Sum of `totient` over an interval integers from `lower` (inclusive)
 ;; to  `upper` (exclusive), optionally in strides of width `stride`.
+
 (define (sum-totient lower upper [stride 1])
   (for/sum ([k (in-range lower upper stride)]) (totient k)))
 
@@ -26,3 +47,21 @@
   (define partial-sums (for/list ([lower lower-bounds])
                          (sum-totient lower upper stride)))
   (for/sum ([s partial-sums]) s))
+
+
+;; warmup
+(define lower 6001)
+(if  (= (chunk-size) 0)
+     (sum-totient lower (+ lower (len)))
+     (if (stride?)
+         (stride-sum-totient lower (len) (chunk-size))
+         (chunk-sum-totient lower (len) (chunk-size))))
+
+;; task
+(when (task?)
+  (if (= (chunk-size) 0)
+      (time (sum-totient lower (+ lower (len))))
+      (if (stride?)
+          (time (sum-totient lower (+ lower (len)) (chunk-size)))
+          (time (sum-totient lower (+ lower (chunk-size)))))))
+     
