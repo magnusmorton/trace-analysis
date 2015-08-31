8e827d0183a4b9acf4062a6e612ef842a3dc2681
Magnus Morton <magnusmorton@icloud.com>
2015-09-01 00:39:29 +0100

    euler runner

---
+++ euler-runner.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/zsh
+sizes=(1 2 4 8 16 32 64 128 256 512 1024 2048 4096)
+lengths=(1000 2000 4000)
+# untransformed
+#warmup
+
+../pycket/pycket-c  sumeuler.rkt -c 0 > trans_benchmarks/eulerseqwu
+
+#task
+for i in {1..10}
+do
+    ../pycket/pycket-c  sumeuler.rkt  -c 0 -t >> trans_benchmarks/eulerseqtask
+done	 
+
+#transformed
+for length in $lengths
+do
+
+    ../pycket/pycket-c  sumeuler.rkt -c 0 -l $length> trans_benchmarks/eulerseqwu
+
+    #task
+    for i in {1..10}
+    do
+	../pycket/pycket-c  sumeuler.rkt  -c 0 -t -l $length >> trans_benchmarks/eulerseqtask
+    done	 
+
+    #echo $length
+    for size in $sizes
+    do
+	if [[ $size -gt $length ]]; then
+	    break
+	fi
+	#warmup
+	../pycket/pycket-c  sumeuler.rkt -c $size -l $length > "trans_benchmarks/eulerchunkwu${length}x${size}"
+	#task
+	for i in {1..10}
+	do
+	    ../pycket/pycket-c  sumeuler.rkt  -c $size -t -l $length >> "trans_benchmarks/eulerchunktask${length}x${size}"
+	done
+
+	#strided
+	../pycket/pycket-c  sumeuler.rkt -c $size -s -l $length > "trans_benchmarks/eulerstridewu${length}x${size}"
+	#task
+	for i in {1..10}
+	do
+	    ../pycket/pycket-c  sumeuler.rkt  -c $size -t -s -l $length >> "trans_benchmarks/eulerstridetask${length}x${size}"
+	done
+	
+    done
+done
