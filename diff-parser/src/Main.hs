7c159c7e0cf860086ca0229b65c7f737bfe94d3d
Magnus Morton <magnusmorton@icloud.com>
2015-09-06 23:50:09 +0100

    arg parsing sort of working

--- diff-parser/src/Main.hs
+++ diff-parser/src/Main.hs
@@ -1,5 +1,24 @@
 module Main where
 
+import Options.Applicative
+import Options.Applicative.Builder
+
+import Control.Monad.Reader
+data Option = Option
+             { size :: Int
+             , file :: String}
+
+parser :: Parser Option
+parser = Option
+         <$> option auto
+         ( long "size"
+           <> metavar "SIZE"
+           <> help "Matrix/euler size" )
+         <*> argument str (metavar "FILE")
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  (Option size file)  <- execParser (info parser fullDesc)
+  putStrLn (show size)
+  putStrLn file
+  
