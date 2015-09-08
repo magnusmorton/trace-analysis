88ba6182d2c7bf683070e49af928bef0551e3540
Magnus Morton <magnusmorton@icloud.com>
2015-09-08 23:48:17 +0100

    flag issues

--- run-mandel.sh
+++ run-mandel.sh
@@ -5,12 +5,12 @@ sizes=(1 2 4 6 8 10 12 16 32 50 64 100 125 250 333 500 1000 2000)
 for n in $ns
 do
     #untransformed warmup
-    ../pycket/pycket-c mandel.rkt -o 0 -n $n > trans_benchmarks/mandelseqwu${n}
+    ../pycket/pycket-c mandel.rkt -c 0 -m $n > trans_benchmarks/mandelseqwu${n}
 
     #task
     for i in {1..10}
     do
-	../pycket/pycket-c mandel.rkt -o 0 -t -n $n >> trans_benchmarks/mandelseqtask${n}
+	../pycket/pycket-c mandel.rkt -c 0 -t -m $n >> trans_benchmarks/mandelseqtask${n}
     done
 
     for size in $sizes
@@ -19,12 +19,12 @@ do
 	    break
 	fi
 	#warmup
-	../pycket/pycket-c mandel.rkt -o $size -n $n > trans_benchmarks/mandeltranswu${n}x${size}
+	../pycket/pycket-c mandel.rkt -c $size -m $n > trans_benchmarks/mandeltranswu${n}x${size}
 
 	#task
 	for i in {1..10}
 	do
-	    ../pycket/pycket-c mandel.rkt -o $size -t -n $n >> trans_benchmarks/mandeltranstask${n}x${size}
+	    ../pycket/pycket-c mandel.rkt -c $size -t -m $n >> trans_benchmarks/mandeltranstask${n}x${size}
 	done
     done
 done
