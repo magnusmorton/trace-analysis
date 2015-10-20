4357b1eec6d79ce97acfbae4071c38e44b53660e
Magnus Morton <m@magnusmorton.com>
2015-10-20 20:27:00 +0100

    fixed mandel sizes

--- diff_mandel.sh
+++ diff_mandel.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 sizes=( 1 2 4 6 8 10 12 16 32 50 64 100 125 250 333 500 1000 2000 4000 8000 )
-dims=( 1000 2000 )
+dims=( 2000 4000 8000 10000)
 chunks=( 0 1 2 )
 DIR=trans_benchmarks
 
